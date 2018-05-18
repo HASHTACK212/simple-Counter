977704cf51f89bfc096fe65e3074061215d14ca6
Harim <mark@wildmile.com>
2018-05-18 15:59:26 -0600

    Add counter

--- active.js
+++ active.js
@@ -2,8 +2,12 @@ document.addEventListener('DOMContentLoaded', function () {
     const bg = chrome.extension.getBackgroundPage();
 
     const count_button = document.getElementById('count_button');
+    const counter = document.getElementById('counter');
 
+    counter.innerHTML = bg.count;
+    
     count_button.addEventListener('click', function () {
         bg.count++;
+        counter.innerHTML = bg.count;
     });
 });
