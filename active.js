fa1f418fd53195b644631b1a7038a8784cca1cb7
Harim <mark@wildmile.com>
2018-05-18 16:18:20 -0600

    Add javascript to make the reset button work

--- active.js
+++ active.js
@@ -2,12 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const bg = chrome.extension.getBackgroundPage();
 
     const count_button = document.getElementById('count_button');
+    const reset_button = document.getElementById('reset');
     const counter = document.getElementById('counter');
 
     counter.innerHTML = bg.count;
     
     count_button.addEventListener('click', function () {
         bg.count++;
+        counter.innerHTML = bg.count;
+    });
+
+    reset_button.addEventListener('click', function () {
+        bg.count = 0;
         counter.innerHTML = bg.count;
     });
 });
